c794e227fe22442b3229614da468bebba80efcc2
Ayushnwoc <75082634+Ayushnwoc@users.noreply.github.com>
2022-08-05 09:55:24 +0530

    Time: 232 ms (17.88%), Space: 49.4 MB (11.19%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,6 +2,34 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
+        sort(coins.begin() , coins.end());
+        vector<vector<int>>dp (n+1, vector<int>(amount + 1 , INT_MAX-1));
+        
+        for(int i = 0 , j = 0;i<=n ;i++)
+        {
+            dp[i][j] = 0;
+        }
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=amount;j++)
+            {
+                dp[i][j] = dp[i-1][j];
+                if(j>=coins[i-1])
+                {
+                    dp[i][j] = min(dp[i][j] , 1 +dp[i][j-coins[i-1]]);
+                }
+            }
+        }
+        if(dp[n][amount] >= INT_MAX - 1)
+        {
+            return -1;
+        }
+        return dp[n][amount];
+    }
+};
+/*
+int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
         sort( coins.begin() , coins.end() );
         long long int ans = 0;
         dp.resize(n+1 , vector<long long int>(amount + 1 , 1e9));
@@ -33,4 +61,4 @@ public:
         return dp[n][amount] = min(dp[n][amount] , findmin(coins , n - 1, amount ));
     }
     vector<vector<long long int>>dp;
-};
+    */
