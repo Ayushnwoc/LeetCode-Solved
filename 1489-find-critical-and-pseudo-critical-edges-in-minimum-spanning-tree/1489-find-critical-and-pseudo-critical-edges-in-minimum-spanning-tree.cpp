85b621217bf201adfd9cfcbe851357c520028581
Ayushnwoc <75082634+Ayushnwoc@users.noreply.github.com>
2022-08-16 12:02:15 +0530

    Time: 159 ms (77.78%), Space: 10.7 MB (86.89%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
@@ -2,13 +2,9 @@
 class Solution {
 public:
     vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
-        cost.resize(edges.size()+1);parent.resize(n);sz.resize(n);make(n);vector<ppi>v;
-        
-        for(int i = 0;i<edges.size();i++)
-        {
+        parent.resize(n);sz.resize(n);make(n);vector<ppi>v;
+        for(int i = 0;i<edges.size();i++){
             v.push_back({edges[i][2] , {edges[i][0] , edges[i][1]}});
-            m[{edges[i][0] , edges[i][1]}] = i;
-            cost[i] = edges[i][2];
         }
         sort(v.begin(), v.end());
         vector<int>ayush(n , 0);
@@ -22,7 +18,7 @@ public:
             int mstree = calculateMST(edges[i][0] , edges[i][1] , n , v);
             make(n);
             int nmstree = edges[i][2] + ncalculateMST(edges[i][0] , edges[i][1] , n , v);
-            //cout<<originMST<<" "<<mstree<<" "<<nmstree<<endl;
+            
             if(originMST < mstree || mstree == -1)
             {
                 critical.push_back(i);
@@ -34,76 +30,56 @@ public:
         }
         return {critical , pseudocritical};
     }
-    int calculateMST(int a , int b , int n , vector<ppi>&v)
-    {
+    int calculateMST(int a , int b , int n , vector<ppi>&v){
         int weight = 0;
-        for(int i = 0;i<v.size();i++)
-        {
-            if(v[i].second.first ==a && v[i].second.second ==b)
-            {
+        for(int i = 0;i<v.size();i++){
+            if(v[i].second.first ==a && v[i].second.second ==b){
                 continue;
             }
-            else
-            {
-                if(!unionfind(v[i].second.first, v[i].second.second))
-                {
+            else{
+                if(!unionfind(v[i].second.first, v[i].second.second)){
                     weight += v[i].first;
                 }
             }
         }
-        for(int i =0;i<n;i++)
-        {
-            if(sz[i] == n) 
-            {
+        for(int i =0;i<n;i++){
+            if(sz[i] == n) {
                 return weight;
             }
         }
         return -1;
     }
-    int ncalculateMST(int a , int b , int n , vector<ppi>&v)
-    {
+    int ncalculateMST(int a , int b , int n , vector<ppi>&v){
         unionfind(a , b);
         int weight = 0;
-        for(int i = 0;i<v.size();i++)
-        {
-            if(v[i].second.first ==a && v[i].second.second ==b)
-            {
+        for(int i = 0;i<v.size();i++){
+            if(v[i].second.first ==a && v[i].second.second ==b){
                 continue;
             }
-            else
-            {
-                if(!unionfind(v[i].second.first, v[i].second.second))
-                {
+            else{
+                if(!unionfind(v[i].second.first, v[i].second.second)){
                     weight += v[i].first;
                 }
             }
         }
-        for(int i =0;i<n;i++)
-        {
-            if(sz[i] == n) 
-            {
+        for(int i =0;i<n;i++){
+            if(sz[i] == n) {
                 return weight;
             }
         }
         return -1;
     }
     private:
-    map<pair<int,int> , int>m;
-    vector<int>cost;
     vector<int>parent;
     vector<int>sz;
-    void make(int n)
-    {
-        for(int i = 0;i<n;i++)
-        {
+    void make(int n){
+        for(int i = 0;i<n;i++){
             parent[i] = i;
             sz[i] = 1;
         }
     }
-    int findset(int v)
-    {
-        if(v == parent[v])
-        {
+    int findset(int v){
+        if(v == parent[v]){
             return v;
         }
         return parent[v] = findset(parent[v]);
