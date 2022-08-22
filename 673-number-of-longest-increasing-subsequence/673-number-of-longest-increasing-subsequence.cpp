0123ab0201daa52850f886f23f46008ca5958165
Ayushnwoc <75082634+Ayushnwoc@users.noreply.github.com>
2022-08-23 01:55:33 +0530

    Time: 304 ms (28.34%), Space: 149.1 MB (5.11%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -1,18 +1,3 @@
-struct hash_pair {
-    template <class T1, class T2>
-    size_t operator()(const pair<T1, T2>& p) const
-    {
-        auto hash1 = hash<T1>{}(p.first);
-        auto hash2 = hash<T2>{}(p.second);
- 
-        if (hash1 != hash2) {
-            return hash1 ^ hash2;             
-        }
-         
-        // If hash1 == hash2, their XOR is zero.
-          return hash1;
-    }
-};
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
