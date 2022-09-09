4ce5288974476f328fe1ef036555b9f2e76d549e
Ayushnwoc <75082634+Ayushnwoc@users.noreply.github.com>
2022-09-10 01:03:08 +0530

    Time: 2 ms (97.03%), Space: 6.6 MB (93.39%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -2,24 +2,13 @@ class Solution {
 public:
     int secondsToRemoveOccurrences(string s) {
         int time = 0;
-        while(true){
-            string temp;
-            int count = 0;
-            for(int i = 0;i<s.length();i++){
-                if(i != 0 && s[i] == '1' && s[i-1] =='0'){
-                    count++;
-                    temp.pop_back();
-                    temp += "10";
-                }
-                else{
-                    temp += s[i];
-                }
+        int z = 0;
+        for(int i =0 ;i<= s.length() - 1 ;i++){
+            z += s[i] == '0';
+            if(z && s[i]=='1'){
+                time = max(time + 1 , z);   
             }
-            s = temp;
-            if(count == 0){
-                return time;
-            }
-            time++;
         }
+        return time;
     }
 };
