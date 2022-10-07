198e20b0bb7d817c04ca5a6194ca2c1f141494e3
Ayushnwoc <75082634+Ayushnwoc@users.noreply.github.com>
2022-10-07 20:06:11 +0530

    Time: 1810 ms (5.00%), Space: 216.1 MB (27.46%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<vector<int> > >dp;
+    int f(int ind , int buy  , int sell , vector<int>&prices){
+        if(sell == 2 || ind == prices.size()){
+            return 0;
+        }
+        if(dp[ind][buy][sell] != INT_MIN){
+            return dp[ind][buy][sell];
+        }
+        if(buy){
+            dp[ind][buy][sell] = max(prices[ind] + f(ind+1 , 0 , sell + 1 , prices) , f(ind+1 , buy , sell , prices));
+            return dp[ind][buy][sell];
+        }
+        dp[ind][buy][sell] = max(-prices[ind] + f(ind+1 , 1 , sell , prices) , f(ind + 1, buy , sell , prices));
+        return dp[ind][buy][sell];
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        dp.resize(n+1 , vector<vector<int>>(2 , vector<int>(2,INT_MIN)));
+        int buy = prices[0];
+        int profit = 0;
+        for(int i = 1 ;i<prices.size() ; i++){
+            if(buy > prices[i]){
+                buy = prices[i];
+            }
+            else if(buy < prices[i]){
+                profit = max(prices[i] - buy , profit);
+            }
+        }
+        int ans = max(-prices[0] + f(1 , 1 , 0 , prices) , f(1 , 0 , 0 , prices));
+        return max(profit , ans);
+    }
+};
